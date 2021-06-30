78c206f699e182c6c77e421d23a4e4b721b61d33
Dawid Woźniak <d.wozniak@merixstudio.com>
2021-06-30 11:36:19 +0200

    repo

---
+++ src/main/java/com/merixstudio/rq2877270/mrpotato/pokemon/UserFavouritesRepository.java
@@ -0,0 +1,6 @@
+package com.merixstudio.rq2877270.mrpotato.pokemon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface UserFavouritesRepository extends JpaRepository<UserFavourites, Long> {
+}
